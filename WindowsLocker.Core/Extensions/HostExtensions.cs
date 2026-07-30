43be524b6c407a63c53296d2aa5c15cc3b0acc3f
Scott Guertin <sguertin@gmail.com>
2026-07-30 18:37:23 -0400

    Publish profile updates, updated extension method to use extension syntax

--- WindowsLocker.Core/Extensions/HostExtensions.cs
+++ WindowsLocker.Core/Extensions/HostExtensions.cs
@@ -8,47 +8,50 @@ namespace WindowsLocker.Core.Extensions;
 
 public static class HostExtensions
 {
-    public static IHostApplicationBuilder AddWindowsLocker(this IHostApplicationBuilder builder)
+    extension(IHostApplicationBuilder builder)
     {
-        builder.Services.AddScoped<IWorkerService, TimedWorkerService>();
-        builder.Services.AddScoped<IDateService, DateService>();
-        builder.Services.AddScoped<ITimeService, TimeService>();
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        public IHostApplicationBuilder AddWindowsLocker()
         {
-            builder.Services.AddScoped<ILockService, WindowsLockService>();
+            builder.Services.AddScoped<IWorkerService, TimedWorkerService>();
+            builder.Services.AddScoped<IDateService, DateService>();
+            builder.Services.AddScoped<ITimeService, TimeService>();
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                builder.Services.AddScoped<ILockService, WindowsLockService>();
+            }
+            else
+            {
+                builder.Services.AddScoped<ILockService, MockLockService>();
+            }
+            builder.Logging.ClearProviders();
+            builder.Logging.AddFileLogger(options =>
+            {
+                options.FilePath = AppContext.BaseDirectory;
+                options.LogLevel = builder.Configuration["Logging:LogLevel:Default"] ?? "Information";
+            });
+            return builder;
         }
-        else
-        {
-            builder.Services.AddScoped<ILockService, MockLockService>();
-        }
-        builder.Logging.ClearProviders();
-        builder.Logging.AddFileLogger(options =>
-        {
-            options.FilePath = AppContext.BaseDirectory;
-            options.LogLevel = builder.Configuration["Logging:LogLevel:Default"] ?? "Information";
-        });
-        return builder;
-    }
 
-    public static IHostApplicationBuilder AddWindowsLockerConsole(this IHostApplicationBuilder builder)
-    {
-        builder.Services.AddScoped<IWorkerService, TimedWorkerService>();
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        public IHostApplicationBuilder AddWindowsLockerConsole()
         {
-            builder.Services.AddScoped<ILockService, WindowsLockService>();
-        }
-        else
-        {
-            builder.Services.AddScoped<ILockService, MockLockService>();
-        }
-        builder.Services.AddScoped<IDateService, DateService>();
-        builder.Services.AddScoped<ITimeService, TimeService>();
+            builder.Services.AddScoped<IWorkerService, TimedWorkerService>();
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                builder.Services.AddScoped<ILockService, WindowsLockService>();
+            }
+            else
+            {
+                builder.Services.AddScoped<ILockService, MockLockService>();
+            }
+            builder.Services.AddScoped<IDateService, DateService>();
+            builder.Services.AddScoped<ITimeService, TimeService>();
         
-        builder.Logging.ClearProviders();
-        builder.Logging.AddConsoleLogger(options =>
-        {
-            options.LogLevel = builder.Configuration["Logging:LogLevel:Default"] ?? "Information";
-        });
-        return builder;
+            builder.Logging.ClearProviders();
+            builder.Logging.AddConsoleLogger(options =>
+            {
+                options.LogLevel = builder.Configuration["Logging:LogLevel:Default"] ?? "Information";
+            });
+            return builder;
+        }
     }
 }
